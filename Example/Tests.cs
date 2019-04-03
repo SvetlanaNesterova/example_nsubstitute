210ec15571a7f2a7c07d9c8c515fd6d47c63c961
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2019-04-04 01:33:36 +0500

    little smaller

--- Example/Tests.cs
+++ Example/Tests.cs
@@ -7,18 +7,12 @@ namespace Example
     [TestFixture]
     public class Tests
     {
-        private IMainModule mainModule;
-
-        [OneTimeSetUp]
-        public void OneTimeSetUp()
-        {
-            mainModule = Substitute.For<IMainModule>();
-            mainModule.Work(Arg.Any<string>(), Arg.Any<Guid>()).Returns(Guid.Empty);
-        }
-
         [Test]
         public void Test()
         {
+            var mainModule = Substitute.For<IMainModule>();
+            mainModule.Work(Arg.Any<string>(), Arg.Any<Guid>()).Returns(Guid.Empty);
+
         }
     }
 }
